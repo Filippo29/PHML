e15a4faa253c2f710c1bf53e01f326b1d6994359
Filippo29 <filodev06@gmail.com>
2026-04-23 12:39:04 +0200

    Added generic MatrixFactory

--- examples/basic_usage.cpp
+++ examples/basic_usage.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 #include "PHML/PHML.hpp"
 #include "PHML/Data/Tensor.hpp"
@@ -6,7 +7,12 @@
 int main() {
     std::cout << "PHML version: "
               << PHML::core::version_string() << '\n';
-    PHML::Data::Matrix<double> mat = PHML::Data::Matrix<double>::random(3, 3);
-    mat.print();
+    PHML::Data::AnyMatrix mat1 = PHML::Data::MatrixFactory::random(100, 100, PHML::Data::DType::Float64);
+    PHML::Data::AnyMatrix mat2 = PHML::Data::MatrixFactory::random(100, 100, PHML::Data::DType::Float64);
+    auto start = std::chrono::high_resolution_clock::now();
+    PHML::Data::Matrix<double> result = std::get<PHML::Data::Matrix<double>>(mat1) * std::get<PHML::Data::Matrix<double>>(mat2);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "Computation time: " << duration.count() << " microseconds" << '\n';
     return 0;
 }
